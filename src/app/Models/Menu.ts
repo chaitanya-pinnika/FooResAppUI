60c7dbafe4181f23dc15504120c2cca0f1e7e9e4
Chaitanya Pinnika <chaitanyapvs4@gmail.com>
2019-08-16 14:08:23 -0400

    Models are added

---
+++ src/app/Models/Menu.ts
@@ -0,0 +1,6 @@
+import {Item} from './item';
+
+export default class Menu{
+    category:string;
+    items:Item[]=new Array();
+}
---
+++ src/app/Models/item.ts
@@ -0,0 +1,7 @@
+export class Item {
+    itemName:string;
+    itemDescription:string;
+    itemPrice:number;
+    itemQuantity:number;
+    itemCategory:string;
+}
