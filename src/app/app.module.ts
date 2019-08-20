690a3cfe3e52937e54c451297a214974cdd1130f
Chaitanya Pinnika <chaitanyapvs4@gmail.com>
2019-08-20 16:32:39 -0400

    Routing Enabled and Home page has been created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    HomeComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path:"",component:HomeComponent},
+      {path:"Orders",component:OrdersComponent}
+      // {path:"Edit",component:}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
